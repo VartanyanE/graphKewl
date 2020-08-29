84d57fad5c94c6dd4972033e1fa7c2990ddbe06a
mikevampm323 <mike.vartanyan@yahoo.com>
2020-08-29 13:40:09 -0700

    built schema

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const { graphqlHTTP } = require("express-graphql");
 const { buildSchema } = require("graphql");
 
 const app = express();
+const events = [];
 
 app.use(bodyParser.json());
 
@@ -11,12 +12,30 @@ app.use(
   "/graphql",
   graphqlHTTP({
     schema: buildSchema(`
+
+
+    type Event {
+      _id: ID!
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+
+    }
+
+    input EventInput {
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
     type RootQuery {
-      events: [String!]!
+      events: [Event!]!
     }
 
     type RootMutation {
-        createEvent(name: String) : String
+        createEvent(eventInput: EventInput) : Event
     }
   
     schema {
@@ -26,11 +45,17 @@ app.use(
   `),
     rootValue: {
       events: () => {
-        return ["ROmantic Cooking", "Sailing"];
+        return events;
       },
       createEvent: (args) => {
-        const eventName = args.name;
-        return eventName;
+        const event = {
+          _id: Math.random().toString(),
+          title: args.title,
+          description: args.description,
+          price: +args.price,
+          date: new Date().toISOString(),
+        };
+        events.push(event);
       },
     },
     graphiql: true,
