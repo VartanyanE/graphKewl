8550ea91ac352c875b9b3a9254d03b8e75388526
mikevampm323 <mike.vartanyan@yahoo.com>
2020-09-09 19:51:06 -0700

    fixed eslint issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,63 @@
-import React, { Component } from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
-import AuthPage from './pages/Auth';
-import BookingsPage from './pages/Bookings';
-import EventsPage from './pages/Events';
-import Navigation from './components/Navigation/Navigation'
-import AuthContext from './context/auth-context'
-import { Token } from 'graphql';
+import React, { Component } from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import AuthPage from "./pages/Auth";
+import BookingsPage from "./pages/Bookings";
+import EventsPage from "./pages/Events";
+import Navigation from "./components/Navigation/Navigation";
+import AuthContext from "./context/auth-context";
+import { Token } from "graphql";
 
 class App extends Component {
-
   state = {
     token: null,
-    userId: null
-  }
+    userId: null,
+  };
   login = (token, userId, tokenExpiration) => {
-    this.setState({ token: token, userId: userId })
-  }
+    this.setState({ token: token, userId: userId });
+  };
 
   logout = () => {
-    this.setState({ token: null, userId: null })
-  }
+    this.setState({ token: null, userId: null });
+  };
 
   render() {
     return (
-
       <Router>
         <>
-          <AuthContext.Provider value={{ token: this.state.token, userId: this.state.userId, login: this.login, logout: this.logout }}>
+          <AuthContext.Provider
+            value={{
+              token: this.state.token,
+              userId: this.state.userId,
+              login: this.login,
+              logout: this.logout,
+            }}
+          >
             <Navigation />
             <main className="main-content">
               <Switch>
-                <Redirect from='/' to="/auth" exact />
-                <Route path="/auth" component={AuthPage} />
+                {!this.state.token && <Redirect from="/" to="/auth" exact />}
+                {this.state.token && <Redirect from="/" to="/events" exact />}
+                {this.state.token && (
+                  <Redirect from="/auth" to="/events" exact />
+                )}
+                {!this.state.token && (
+                  <Route path="/auth" component={AuthPage} />
+                )}
                 <Route path="/events" component={EventsPage} />
-                <Route path="/bookings" component={BookingsPage} />
+                {this.state.token && (
+                  <Route path="/bookings" component={BookingsPage} />
+                )}
               </Switch>
             </main>
           </AuthContext.Provider>
         </>
-      </Router >
+      </Router>
     );
   }
 }
