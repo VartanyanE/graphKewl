4ff801b36b93184e4531ce61fdd9a15f7a8fca10
mikevampm323 <mike.vartanyan@yahoo.com>
2020-09-16 20:58:38 -0700

    finally works

--- client/src/pages/Events.js
+++ client/src/pages/Events.js
@@ -1,51 +1,54 @@
 import React, { Component } from "react";
 
 class EventsPage extends React.Component {
-    state = {
-        compliments: []
-    }
-    componentDidMount() {
-
-        let requestBody = {
-            query: `
+  state = {
+    compliments: [],
+  };
+  componentDidMount() {
+    this.fetchCompliments();
+  }
+  async fetchCompliments() {
+    let requestBody = {
+      query: `
       query {
         compliments {
           compliment
         }
       }
           `,
-        };
+    };
 
-        fetch("http://localhost:8000/graphql", {
-            method: "POST",
-            body: JSON.stringify(requestBody),
-            headers: {
-                "Content-Type": "application/json",
-            },
-        })
-            .then((res) => {
-                if (res.status !== 200 && res.status !== 201) {
-                    throw new Error("Failed!");
-                }
-                return res.json();
-            })
-            .then((resData) => {
-                return this.setState({ compliments: resData });
-                console.log(this.state);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-        console.log(this.state)
-    }
-    render() {
-        return (
-            <div>
-                {/* {this.state.compliments.map((item) => (
-                    <h1>{item.compliment}</h1>
-                ))} */}
-            </div>
-        );
-    }
+    await fetch("http://localhost:8000/graphql", {
+      method: "POST",
+      body: JSON.stringify(requestBody),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error("Failed!");
+        }
+        return res.json();
+      })
+      .then((resData) => {
+        this.setState({ compliments: resData.data.compliments });
+        console.log(this.state);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    console.log(this.state);
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.compliments.map((item) => (
+          <h1>{item.compliment}</h1>
+        ))}
+      </div>
+    );
+  }
 }
 export default EventsPage;
