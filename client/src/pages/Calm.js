a0601fc4ffa6c1b6f75fdc053fbe1dda0a6c7b8e
mikevampm323 <mike.vartanyan@yahoo.com>
2020-10-12 00:31:29 -0700

    joke API

--- client/src/pages/Calm.js
+++ client/src/pages/Calm.js
@@ -6,14 +6,8 @@ class Calm extends React.Component {
     compliments: [],
     randomComp: [],
     horoscope: [],
-
+    joke: [],
   };
-
-
-
-
-
-
 
   fetchCompliments = async () => {
     let requestBody = {
@@ -41,36 +35,29 @@ class Calm extends React.Component {
       })
       .then((resData) => {
         this.setState({ compliments: resData.data.compliments });
-
       })
       .catch((err) => {
         console.log(err);
       });
 
-
     this.state.compliments.map((item) => {
-      this.state.compliments.sort(() => Math.random() - Math.random())
+      this.state.compliments
+        .sort(() => Math.random() - Math.random())
         .find(() => true);
 
       this.setState({ randomComp: [item] });
-
-
-
-    }
-
-    )
+    });
   };
 
   selectSign = () => {
-    const sign = document.getElementById('signs')
+    const sign = document.getElementById("signs");
     const selectedSign = sign.options[sign.selectedIndex].value;
-    this.setState({ signs: selectedSign })
-    console.log(selectedSign)
-  }
+    this.setState({ signs: selectedSign });
+    console.log(selectedSign);
+  };
 
   fetchHoroscope = async () => {
-    console.log(this.state)
-
+    console.log(this.state);
 
     // await fetch(`https://horoscope5.p.rapidapi.com/general/daily?sign=${this.state.signs}`, {
     //   "method": "GET",
@@ -88,43 +75,58 @@ class Calm extends React.Component {
     //   .catch(err => {
     //     console.log(err);
     //   });
+  };
 
-
-  }
-
+  fetchJoke = () => {
+    fetch(
+      "https://webknox-jokes.p.rapidapi.com/jokes/random?maxLength=100&minRating=8",
+      {
+        method: "GET",
+        headers: {
+          "x-rapidapi-host": "webknox-jokes.p.rapidapi.com",
+          "x-rapidapi-key":
+            "a55981e29amsh474209918c2f0eap1b8fb4jsn84b08394e42a",
+        },
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((res) => {
+        this.setState({ joke: [res.joke] });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   render() {
-
-
-
     return (
-
       <div>
         <button onClick={this.fetchCompliments}>GIVE ME A COMPLIMENT</button>
 
         {this.state.randomComp.map((item, index) => (
-          <h5 key={index} >{item.compliment}</h5>
+          <h5 key={index}>{item.compliment}</h5>
         ))}
-        <div> <label for="signs">What is your sign?:</label>
-
+        <div>
+          {" "}
+          <label for="signs">What is your sign?:</label>
           <select name="signs" id="signs" onChange={this.selectSign}>
             <option value=""></option>
-            <option value="virgo"  >Virgo</option>
+            <option value="virgo">Virgo</option>
             <option value="cancer">Cancer</option>
             <option value="gemini">Gemini</option>
             <option value="leo">Leo</option>
             <option value="scorpio">Scorpio</option>
-
-
           </select>
-
           <button onClick={this.fetchHoroscope}>GIVE ME MY HOROSCOPE</button>
           {this.state.horoscope.map((item, index) => (
-            <h5 key={index} >{item}</h5>
+            <h5 key={index}>{item}</h5>
           ))}
-
-
-
+          <button onClick={this.fetchJoke}>TELL ME A JOKE</button>
+          {this.state.joke.map((item, index) => (
+            <h5 key={index}>{item}</h5>
+          ))}
         </div>
       </div>
     );
