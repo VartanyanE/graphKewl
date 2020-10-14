4bd1e4f88b0a35b257f9fce1a6cacf39233e4b75
VartanyanE <mike.vartanyan@yahoo.com>
2020-10-13 21:04:42 -0700

    added a framework

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -2,15 +2,17 @@ import React, { Component } from "react";
 import { render } from "react-dom";
 import "./Landing.css";
 import video from "./rain.mp4";
+import { Button } from '@material-ui/core';
 
 class Landing extends Component {
   render() {
     return (
       <div className="container">
-        <h1>The Bookking Page</h1>
+
         <video autoPlay muted loop id="myVideoStop">
           <source src={video} type="video/mp4" />
         </video>
+        <Button color="primary">Hello World</Button>
       </div>
     );
   }
